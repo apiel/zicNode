577717224b5ea3bd1b7bb6fd91cc6e07af617abd
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-01-10 16:42:27 +0100

    getAudoDeviceInfo

--- src/zic.cc
+++ src/zic.cc
@@ -1,27 +1,35 @@
 #include <napi.h>
-
 #include <RtAudio.h>
-#include <iostream>
 
-Napi::String getAudoDeviceInfo(const Napi::CallbackInfo& info)
+Napi::Array getAudoDeviceInfo(const Napi::CallbackInfo& info)
 {
+    Napi::Env env = info.Env();
+
     RtAudio audio;
-    // Determine the number of devices available
-    unsigned int devices = audio.getDeviceCount();
-    // // Scan through devices for various capabilities
+    unsigned int deviceCount = audio.getDeviceCount();
+
+    Napi::Array devices = Napi::Array::New(env);
     RtAudio::DeviceInfo rtInfo;
-    for (unsigned int i = 0; i < devices; i++) {
+    for (unsigned int i = 0; i < deviceCount; i++) {
         rtInfo = audio.getDeviceInfo(i);
         if (rtInfo.probed == true) {
-            // Print, for example, the maximum number of output channels for each device
-            std::cout << "device = " << i;
-            std::cout << ": name = " << rtInfo.name << " ";
-            std::cout << ": maximum output channels = " << rtInfo.outputChannels << "\n";
+            Napi::Object device = Napi::Object::New(env);
+            device.Set("probed", rtInfo.probed);
+            device.Set("name", rtInfo.name);
+            device.Set("outputChannels", rtInfo.outputChannels);
+            device.Set("inputChannels", rtInfo.inputChannels);
+            device.Set("duplexChannels", rtInfo.duplexChannels);
+            device.Set("isDefaultOutput", rtInfo.isDefaultOutput);
+            device.Set("isDefaultInput", rtInfo.isDefaultInput);
+            // device.Set("sampleRates", rtInfo.sampleRates);
+            device.Set("preferredSampleRate", rtInfo.preferredSampleRate);
+            // device.Set("nativeFormats", rtInfo.nativeFormats);
+
+            devices.Set(i, device);
         }
     }
 
-    Napi::Env env = info.Env();
-    return Napi::String::New(env, "world 2");
+    return devices;
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
@@ -31,3 +39,16 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
 }
 
 NODE_API_MODULE(hello, Init)
+
+// typedef struct RtAudio::DeviceInfo {
+//   bool probed;                           // true if the device capabilities were successfully probed.
+//   std::string name;                      // Character string device identifier.
+//   unsigned int outputChannels;           // Maximum output channels supported by device.
+//   unsigned int inputChannels;            // Maximum input channels supported by device.
+//   unsigned int duplexChannels;           // Maximum simultaneous input/output channels supported by device.
+//   bool isDefaultOutput;                  // true if this is the default output device.
+//   bool isDefaultInput;                   // true if this is the default input device.
+//   std::vector<unsigned int> sampleRates; // Supported sample rates.
+//   unsigned int preferredSampleRate;      // Preferred sample rate, e.g. for WASAPI the system sample rate.
+//   RtAudioFormat nativeFormats;           // Bit mask of supported data formats.
+// };
