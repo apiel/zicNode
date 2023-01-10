828bee9f6854a71c378e46f61a2d62094ec48a2e
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-01-10 19:22:50 +0100

    audio

--- src/zic_start.h
+++ src/zic_start.h
@@ -6,59 +6,49 @@
 
 #include <unistd.h>
 
-typedef float MY_TYPE;
+#ifndef SAMPLE_RATE
+#define SAMPLE_RATE 44100
+#endif
+
 #define FORMAT RTAUDIO_FLOAT32
-#define SCALE 1.0
-#define BASE_RATE 0.005
-#define TIME   1.0
 
-void errorCallback(RtAudioError::Type type, const std::string& errorText)
-{
-    // This example error handling function does exactly the same thing
-    // as the embedded RtAudio::error() function.
-    std::cout << "in errorCallback" << std::endl;
-    if (type == RtAudioError::WARNING)
-        std::cerr << '\n'
-                  << errorText << "\n\n";
-    else if (type != RtAudioError::WARNING)
-        throw(RtAudioError(errorText, type));
-}
+unsigned int channels;
 
-int saw(void* outputBuffer, void* /*inputBuffer*/, unsigned int nBufferFrames,
+int audioCallback(void* outputBuffer, void* /*inputBuffer*/, unsigned int nBufferFrames,
     double /*streamTime*/, RtAudioStreamStatus status, void* data)
 {
     unsigned int i, j;
     // extern unsigned int channels;
-    unsigned int channels = 2; // FIXME
-    MY_TYPE* buffer = (MY_TYPE*)outputBuffer;
+    // unsigned int channels = 2; // FIXME
+    float* buffer = (float*)outputBuffer;
     double* lastValues = (double*)data;
 
     if (status)
         std::cout << "Stream underflow detected!" << std::endl;
 
+    const float BASE_RATE = 0.005;
     double increment;
     for (j = 0; j < channels; j++) {
         increment = BASE_RATE * (j + 1 + (j * 0.1));
         for (i = 0; i < nBufferFrames; i++) {
-            *buffer++ = (MY_TYPE)(lastValues[j] * SCALE * 0.5);
+            *buffer++ = (float)(lastValues[j] * 0.5);
             lastValues[j] += increment;
             if (lastValues[j] >= 1.0)
                 lastValues[j] -= 2.0;
         }
     }
-
     return 0;
 }
 
 class ZicWorker : public Napi::AsyncWorker {
 protected:
     RtAudio::StreamOptions options;
-    int points;
+    unsigned int deviceId;
 
 public:
-    ZicWorker(Napi::Function& callback, int points)
+    ZicWorker(Napi::Function& callback, unsigned int deviceId)
         : Napi::AsyncWorker(callback)
-        , points(points)
+        , deviceId(deviceId)
     {
     }
     ~ZicWorker() { }
@@ -66,32 +56,31 @@ public:
     void Execute()
     {
         RtAudio audio;
-        if (audio.getDeviceCount() < 1) {
+        unsigned int deviceCount = audio.getDeviceCount();
+        if (deviceCount < 1) {
             SetError("No audio devices found");
             return;
         }
 
-        // Set our stream parameters for output only.
+        if (deviceId > deviceCount - 1) {
+            deviceId = audio.getDefaultOutputDevice();
+            // std::cout << "get default device: " << deviceId << std::endl;
+        }
+
         unsigned int bufferFrames = 512;
         RtAudio::StreamParameters oParams;
         oParams.deviceId = 0;
-        if (oParams.deviceId == 0)
-            oParams.deviceId = audio.getDefaultOutputDevice();
 
-        oParams.nChannels = 2;
-        // oParams.firstChannel = offset;
+        // RtAudio::DeviceInfo rtInfo = audio.getDeviceInfo(oParams.deviceId);
+        // std::cout << "rtInfo.name: " << rtInfo.name << std::endl;
+        // oParams.nChannels = rtInfo.outputChannels;
+        oParams.nChannels = 1; // Might want to use only one channel
 
-        options.flags = RTAUDIO_HOG_DEVICE;
-        options.flags |= RTAUDIO_SCHEDULE_REALTIME;
-#if !defined(USE_INTERLEAVED)
-        options.flags |= RTAUDIO_NONINTERLEAVED;
-#endif
+        channels = oParams.nChannels;
 
         double* data = (double*)calloc(oParams.nChannels, sizeof(double));
-        // unsigned int sampleRate;
         try {
-            audio.openStream(&oParams, NULL, FORMAT, 44100, &bufferFrames, &saw, (void*)data, &options, &errorCallback);
-            // here we could send the sampleRate to the main thread
+            audio.openStream(&oParams, NULL, FORMAT, SAMPLE_RATE, &bufferFrames, &audioCallback, (void*)data);
             audio.startStream();
         } catch (RtAudioError& e) {
             e.printMessage();
