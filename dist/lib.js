1bf141a424737b61157701decdaf901847248ea5
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-01-17 18:03:55 +0100

    fix tsconfig

--- dist/lib.js
+++ dist/lib.js
@@ -3,7 +3,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-var bindings_1 = __importDefault(require("bindings"));
-exports.hello = (0, bindings_1.default)('hello');
-//# sourceMappingURL=lib.js.map
+exports.trackSetPath = exports.SynthPathIds = exports.PdPathIds = exports.trackCc = exports.trackNoteOff = exports.trackNoteOn = exports.getSequencerStates = exports.setSequencerState = exports.getPattern = exports.setPatternStep = exports.getPatternLength = exports.setPatternLength = exports.setBpm = exports.getBpm = exports.isAudioRunning = exports.stop = exports.start = exports.getAudoDeviceInfo = exports.NOTE_END = exports.NOTE_START = exports.SAMPLE_RATE = exports.TRACK_COUNT = exports.MAX_VOICES_IN_PATTERN = exports.MAX_STEPS_IN_PATTERN = exports.PATTERN_COUNT = void 0;
+const bindings_1 = __importDefault(require("bindings"));
+const zic = (0, bindings_1.default)('zic');
+exports.PATTERN_COUNT = zic.PATTERN_COUNT;
+exports.MAX_STEPS_IN_PATTERN = zic.MAX_STEPS_IN_PATTERN;
+exports.MAX_VOICES_IN_PATTERN = zic.MAX_VOICES_IN_PATTERN;
+exports.TRACK_COUNT = zic.TRACK_COUNT;
+exports.SAMPLE_RATE = zic.SAMPLE_RATE;
+exports.NOTE_START = zic.NOTE_START;
+exports.NOTE_END = zic.NOTE_END;
+exports.getAudoDeviceInfo = zic.getAudoDeviceInfo;
+exports.start = zic.start;
+exports.stop = zic.stop;
+exports.isAudioRunning = zic.isAudioRunning;
+exports.getBpm = zic.getBpm;
+exports.setBpm = zic.setBpm;
+exports.setPatternLength = zic.setPatternLength;
+exports.getPatternLength = zic.getPatternLength;
+exports.setPatternStep = zic.setPatternStep;
+exports.getPattern = zic.getPattern;
+exports.setSequencerState = zic.setSequencerState;
+exports.getSequencerStates = zic.getSequencerStates;
+// trackIndex, note, velocity, (voice=0)
+exports.trackNoteOn = zic.trackNoteOn;
+exports.trackNoteOff = zic.trackNoteOff;
+exports.trackCc = zic.trackCc;
+var PdPathIds;
+(function (PdPathIds) {
+    PdPathIds[PdPathIds["Patch"] = 0] = "Patch";
+})(PdPathIds = exports.PdPathIds || (exports.PdPathIds = {}));
+var SynthPathIds;
+(function (SynthPathIds) {
+    SynthPathIds[SynthPathIds["Osc"] = 0] = "Osc";
+    SynthPathIds[SynthPathIds["Lfo1"] = 1] = "Lfo1";
+    SynthPathIds[SynthPathIds["Lfo2"] = 2] = "Lfo2";
+})(SynthPathIds = exports.SynthPathIds || (exports.SynthPathIds = {}));
+exports.trackSetPath = zic.trackSetPath;
--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,57 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var lib_1 = require("./lib");
-console.log((0, lib_1.hello)());
-//# sourceMappingURL=main.js.map
+const process_1 = require("process");
+const lib_1 = require("./lib");
+console.log('Zic node');
+// export const PATTERN_COUNT: number = zic.PATTERN_COUNT;
+// export const MAX_STEPS_IN_PATTERN: number = zic.MAX_STEPS_IN_PATTERN;
+// export const MAX_VOICES_IN_PATTERN: number = zic.MAX_VOICES_IN_PATTERN;
+// export const TRACK_COUNT: number = zic.TRACK_COUNT;
+// export const SAMPLE_RATE: number = zic.SAMPLE_RATE;
+// export const NOTE_START: number = zic.NOTE_START;
+// export const NOTE_END: number = zic.NOTE_END;
+console.log('DEFINE VAR', {
+    PATTERN_COUNT: lib_1.PATTERN_COUNT,
+    MAX_STEPS_IN_PATTERN: lib_1.MAX_STEPS_IN_PATTERN,
+    MAX_VOICES_IN_PATTERN: lib_1.MAX_VOICES_IN_PATTERN,
+    TRACK_COUNT: lib_1.TRACK_COUNT,
+    SAMPLE_RATE: lib_1.SAMPLE_RATE,
+    NOTE_START: lib_1.NOTE_START,
+    NOTE_END: lib_1.NOTE_END,
+});
+console.log('getAudoDeviceInfo', (0, lib_1.getAudoDeviceInfo)());
+console.log('Current Bpm', (0, lib_1.getBpm)());
+(0, lib_1.setBpm)(90);
+console.log('New Bpm', (0, lib_1.getBpm)());
+(0, lib_1.start)(123);
+console.log('next Bpm', (0, lib_1.getBpm)());
+(0, lib_1.trackSetPath)(0, '/home/alex/Music/zicJs/zicNode/data/instruments/pd/02_kick');
+(0, lib_1.trackSetPath)(1, '/home/alex/Music/zicJs/zicNode/data/wavetables/0_test.wav', lib_1.SynthPathIds.Osc);
+(0, lib_1.trackSetPath)(1, '/home/alex/Music/zicJs/zicNode/data/wavetables/0_test.wav', lib_1.SynthPathIds.Lfo1);
+(0, lib_1.trackSetPath)(1, '/home/alex/Music/zicJs/zicNode/data/wavetables/0_test.wav', lib_1.SynthPathIds.Lfo2);
+(0, lib_1.setPatternLength)(0, 4);
+console.log('pattern length', (0, lib_1.getPatternLength)(0));
+(0, lib_1.setPatternStep)(0, 0, 60, 100, false);
+(0, lib_1.setSequencerState)(0, 0, 0, true);
+(0, lib_1.setPatternLength)(1, 8);
+(0, lib_1.setPatternStep)(1, 2, 70, 100, false);
+(0, lib_1.setPatternStep)(1, 6, 80, 100, false);
+console.log('pattern', JSON.stringify((0, lib_1.getPattern)(1), null, 4));
+(0, lib_1.setSequencerState)(1, 1, 0, true);
+console.log('Get sequencer state track 2', (0, lib_1.getSequencerStates)(1));
+// Test error handling
+// try {
+//     console.log('pattern length', getPatternLength(2000));
+// } catch (error) {
+//     console.log('error', error);
+// }
+setInterval(() => {
+    if (!(0, lib_1.isAudioRunning)()) {
+        (0, process_1.exit)();
+    }
+    console.log('keep alive');
+}, 1000);
+(0, lib_1.trackNoteOn)(0, 60, 100);
+setTimeout(() => {
+    (0, lib_1.trackNoteOff)(0, 60);
+}, 1000);
